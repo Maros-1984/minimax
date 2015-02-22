f53c393c890f6f62e12c7afb632ccce1d94f7d86
Maros <maros@vranec.com>
2015-02-22 12:38:30 +0100

    Alpha-beta pruning added.

--- src/main/java/com/vranec/minimax/ArtificialIntelligence.java
+++ src/main/java/com/vranec/minimax/ArtificialIntelligence.java
@@ -2,15 +2,35 @@ package com.vranec.minimax;
 
 public class ArtificialIntelligence {
     public BestMove negamax(Board board, int depth, Color color) {
+        return negamax(board, depth, -Integer.MAX_VALUE, Integer.MAX_VALUE, color);
+    }
+
+    /**
+     * 
+     * @param board
+     *            Current board state.
+     * @param depth
+     *            Depth to search in.
+     * @param color
+     *            Who is on the move.
+     * @return
+     */
+    public BestMove negamax(Board board, int depth, int alpha, int beta, Color color) {
         if (depth == 0 || board.isGameOver()) {
             return new BestMove(board.getBoardValue(color));
         }
-        BestMove bestMove = new BestMove(Integer.MIN_VALUE);
-        for (Board nextBoard : board.getNextBoards(color.getOtherColor())) {
-            int val = -negamax(nextBoard, depth - 1, color.getOtherColor()).getValue();
+        BestMove bestMove = new BestMove(-Integer.MAX_VALUE);
+        for (Board nextBoard : board.getNextBoards(color)) {
+            BestMove nextBestMove = negamax(nextBoard, depth - 1, -beta, -alpha, color.getOtherColor());
+            int val = -nextBestMove.getValue();
             if (val > bestMove.getValue()) {
+                bestMove = nextBestMove;
                 bestMove.setValue(val);
                 bestMove.setBestBoard(nextBoard);
+                alpha = Math.max(alpha, val);
+            }
+            if (alpha >= beta) {
+                break;
             }
         }
         return bestMove;
