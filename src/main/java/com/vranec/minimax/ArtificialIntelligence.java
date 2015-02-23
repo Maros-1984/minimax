3e21b3dc8a025de6b069938172e58fd9cc920b0a
Maros <maros@vranec.com>
2015-02-23 23:30:41 +0100

    Kind of iterative deepening added.

--- src/main/java/com/vranec/minimax/ArtificialIntelligence.java
+++ src/main/java/com/vranec/minimax/ArtificialIntelligence.java
@@ -1,5 +1,10 @@
 package com.vranec.minimax;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
 
@@ -8,7 +13,10 @@ public class ArtificialIntelligence {
             .recordStats().build();
 
     public BestMove getBestMove(Board board, int depth, Color color) {
-        BestMove alphaBeta = alphaBeta(board, depth, -Integer.MAX_VALUE, Integer.MAX_VALUE, color);
+        BestMove alphaBeta = null;
+        for (int i = 1; i <= depth; i++) {
+            alphaBeta = alphaBeta(board, i, -Integer.MAX_VALUE, Integer.MAX_VALUE, color);
+        }
         System.out.println(transpositionTable.stats());
         return alphaBeta;
     }
@@ -49,6 +57,25 @@ public class ArtificialIntelligence {
         }
 
         BestMove bestMove = new BestMove(-Integer.MAX_VALUE);
+        List<Board> possibleMoves = new ArrayList<Board>();
+        for (Board nextBoard : board.getNextBoards(color)) {
+            possibleMoves.add(nextBoard);
+        }
+        Collections.sort(possibleMoves, new Comparator<Board>() {
+            public int compare(Board o1, Board o2) {
+                Integer o1value = Integer.MIN_VALUE, o2value = Integer.MIN_VALUE;
+                TranspositionTableEntry o1tt = transpositionTable.getIfPresent(o1);
+                if (o1tt != null) {
+                    o1value = o1tt.getBestMove().getValue();
+                }
+                TranspositionTableEntry o2tt = transpositionTable.getIfPresent(o2);
+                if (o2tt != null) {
+                    o2value = o2tt.getBestMove().getValue();
+                }
+
+                return Integer.compare(o1value, o2value);
+            }
+        });
         for (Board nextBoard : board.getNextBoards(color)) {
             BestMove nextBestMove = alphaBeta(nextBoard, depth - 1, -beta, -alpha, color.getOtherColor());
             int val = -nextBestMove.getValue();
